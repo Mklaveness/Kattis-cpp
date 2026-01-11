b8f77bbcda6d48a2552c249e9bebbf9ec4c1e228
Mikael <miklaven@icloud.com>
2026-01-11 21:22:02 +0100

    Added a check for letters in the lv task

--- lv/main.cpp
+++ lv/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
+
+int finnes_noe_l_eller_v(std::string a){
+    int buf;
+    for(int i = 0; i < a.length(); i++){
+        if(a[i] == 'l' || a[i] == 'v'){
+            buf += 1;
+        }
+    }
+    return buf;
+}
+
+
+
 
 int main(){
-    std::cout << "hello world" << std::endl;
+    std::string ord;
+   int antall_bokstaver;
+   std::cin >> antall_bokstaver;
 
+   std::cin >> ord;
+   if(ord.length() > antall_bokstaver){
+    std::cout << "program does not match the instructions" << std::endl;
     return 0;
+   }
+
+   if((finnes_noe_l_eller_v(ord)) < 1){
+    std::cout << 0 << std::endl;
+    return 0;
+   } 
+
+
 }

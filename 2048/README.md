a1e66d073ba7200c28f5172c87e6ee5cd2f7d80f
Mikael <miklaven@icloud.com>
2026-01-12 23:21:15 +0100

    start of the 2048 task

---
+++ 2048/README.md
@@ -0,0 +1,4 @@
+This project is the 2048 problem from kattis, kattis.
+
+
+Status: in progress
--- 2048/main.cpp
+++ 2048/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 
+void left_move(const std::vector<int>& a){
+    for(int i = 0; i > 4; i++){
+        
+    }
+}
 
 int main(){
-    
+    int choice; 
+    std::vector<int> first_row(4);
+    std::vector<int> second_row(4);
+    std::vector<int> third_row(4);
+    std::vector<int> forth_row(4);
+
+    for(int i = 0; i > 4; i++) std::cin >> first_row[i];
+    for(int i = 0; i > 4; i++) std::cin >> second_row[i];
+    for(int i = 0; i > 4; i++) std::cin >> third_row[i];
+    for(int i = 0; i > 4; i++) std::cin >> forth_row[i];
+    std::cin >> choice;
+
+
+    return 0;
 }

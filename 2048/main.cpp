394d4319d839acbfad310466bff90e8d119eaf8d
Mikael <miklaven@icloud.com>
2026-01-14 00:18:06 +0100

    fixed bugs whit out of bounds functions

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -23,7 +23,7 @@ void sette_nullbak_v(int a[4]) {
 
 void plussesammen_v(int b[4]) {
     for (int i = 0; i < 3; i++) {
-        if (b[i] == b[i+1]) {
+        if (b[i] == b[i+1] && b[i] != 0)  {
             b[i] = (b[i] * 2);
             b[i+1] = 0;
         }
@@ -31,7 +31,7 @@ void plussesammen_v(int b[4]) {
 }
 
 void sette_nullbak_h(int a[4]) {
-    for (int i = 3; i >= 0; i--) {
+    for (int i = 3; i > 0; i--) {
         if (a[3] == 0){
             a[3] = a[2];
             a[2] = a[1];
@@ -43,8 +43,8 @@ void sette_nullbak_h(int a[4]) {
             a[1] = a[0];
             a[0] = 0;
         }
-        else if (a[2] == 0) {
-            a[2] = a[1];
+        else if (a[1] == 0) {
+            a[1] = a[0];
             a[0] = 0;
         }
     }
@@ -53,8 +53,8 @@ void sette_nullbak_h(int a[4]) {
 
 
 void plussesammen_h(int b[4]) {
-    for (int i = 3; i >= 0; i--) {
-        if (b[i] == b[i-1]) {
+    for (int i = 3; i > 0; i--) {
+        if (b[i] == b[i-1] && b[i] != 0) {
             b[i] = (b[i] * 2);
             b[i-1] = 0;
         }
@@ -152,26 +152,22 @@ int main(){
             sette_nullbak_h(forth_row);
             plussesammen_h(forth_row);
             sette_nullbak_h(forth_row);
-
+            
             for (int i = 0; i < 4; i++) {
-                std::cout << first_row[i] << " ";
+            std::cout << first_row[i] << " ";
+          }
+            std::cout << "\n";
+            for (int i = 0; i < 4; i++) {
+            std::cout << second_row[i] << " ";
             }
             std::cout << "\n";
-
             for (int i = 0; i < 4; i++) {
-                std::cout << second_row[i] << " ";
+            std::cout << third_row[i] << " ";
             }
             std::cout << "\n";
-
             for (int i = 0; i < 4; i++) {
-                std::cout << third_row[i] << " ";
+            std::cout << forth_row[i] << " ";
             }
-            std::cout << "\n";
-
-            for (int i = 0; i < 4; i++) {
-                std::cout << forth_row[i] << " ";
-            }
-            std::cout << "\n";
         }
         else if(choice == 3){
             redo_rows(first_row, second_row, third_row, forth_row, flipped_row1, flipped_row2, flipped_row3, flipped_row4);

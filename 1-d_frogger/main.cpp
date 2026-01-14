411c799963d0c1a78f7c0c22c5cfee245c4f5119
Mikael <miklaven@icloud.com>
2026-01-14 16:35:12 +0100

    non-woriking model

---
+++ 1-d_frogger/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int main(){
+    int board_squares;
+    int starting_square;
+    int magic_number;
+    
+
+    std::vector<int> numbers_insquare(board_squares);
+
+    std::cin >> board_squares >> starting_square >> magic_number; 
+    for(int i = 0; i < board_squares; i++){
+        std::cin >> numbers_insquare[i];
+    }
+
+    int indx = starting_square - 1;
+    int cycle = indx;
+    int amount_of_moves = 0;
+
+    while(1){
+        if(numbers_insquare[indx] == magic_number){
+            std::cout << "magic" << "\n" << amount_of_moves;
+            break;
+        }
+        else if(indx < 0){
+            std::cout << "left" << "\n" << amount_of_moves;
+            break;
+        }
+        else if(indx > board_squares - 1){
+            std::cout << "right" << "\n" << amount_of_moves;
+            break;
+        }
+        else if(numbers_insquare[indx] != magic_number && numbers_insquare[indx] < 0){
+            indx = indx - std::abs(numbers_insquare[indx]);
+            amount_of_moves++;
+        }
+        else if(numbers_insquare[indx] != magic_number){
+            indx = indx + std::abs(numbers_insquare[indx]);
+            amount_of_moves++;
+        }
+        else if(indx == cycle){
+            std::cout << "cycle" << "\n" << amount_of_moves;
+        }
+        
+    }        
+return 0;
+}

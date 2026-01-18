f9bfdbadd5721e9e4c624a2689fba21fe244abc4
Mikael <miklaven@icloud.com>
2026-01-18 21:34:10 +0100

    added initial start of new project

---
+++ draft_time/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+
+int main(){
+    int nfl_teams, draft_rounds, players;
+    std::cin >> nfl_teams >> draft_rounds >> players;
+
+    std::map<int, std::string> team_presferences;
+    std::map<int, std::string> player_preferences;
+   
+    for(int i = 0; i < nfl_teams; i++){
+        
+    }
+
+
+}

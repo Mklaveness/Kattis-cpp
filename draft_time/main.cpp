12d03d527e38e27cd252a6a27b346350d408ce62
Mikael <miklaven@icloud.com>
2026-02-07 18:54:26 +0100

    added a functional input for team and playeres

--- draft_time/main.cpp
+++ draft_time/main.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
+#include <string>
 #include <map>
+#include <sstream>
+
+class player
+{    
+public:
+    std::string name;
+    std::map<int, std::string> team_wishes;
+};
+
+class team{
+public:
+    std::string teamname;
+    std::map<int, std::string> player_wishes;
+};
+
 
 int main(){
-    int nfl_teams, draft_rounds, players;
-    std::cin >> nfl_teams >> draft_rounds >> players;
+    std::map<int, player> playerMap;
+    std::map<int, team> teamMap;
+    int teams, max_player, players;
+    std::cin >> teams >> max_player >> players;
+    std::string buffer_string;
 
-    std::map<int, std::string> team_presferences;
-    std::map<int, std::string> player_preferences;
-   
-    for(int i = 0; i < nfl_teams; i++){
-        
+
+    for(int i = 1; i <= teams; i++){
+        std::getline(std::cin >> std::ws, buffer_string);
+        std::stringstream ss(buffer_string);
+        std::string teamnames;
+        ss >> teamnames;
+        teamMap[i].teamname = teamnames;
+
+         for (int n = 1; n <= players; n++) {
+            std::string playerName;
+            if (!(ss >> playerName)) break;
+            teamMap[i].player_wishes[n] = playerName;
+        }
     }
 
+    for(int i = 1; i <= players; i++){
+        std::getline(std::cin >> std::ws, buffer_string);
+        std::stringstream ss(buffer_string);
+        std::string playernames;
+        ss >> playernames;
+        playerMap[i].name = playernames;
 
+        for(int n = 1; n <= teams; n++){
+            std::string teamnames;
+            if(!(ss >> teamnames)) break;
+            playerMap[i].team_wishes[n] = teamnames;
+        }
+    }
+     
+    return 0;
 }
